694de30336f3662afb31dd7793195f286c5b38ca
Zehab-bid <zehabidha152gmail.com>
2025-05-01 14:21:08 +0700

    proges 3

--- tambahan.cpp
+++ tambahan.cpp
@@ -7,4 +7,23 @@ private:
         return 0.5 * a * t;
     }
 
-}
+    public:
+    int alas, tinggi;
+
+    void inputData() {
+        cout << "Masukkan Alas Segitiga: ";
+        cin >> alas;
+        cout << "Masukkan Tinggi Segitiga: ";
+        cin >> tinggi;
+
+    }
+
+    void outputData() {
+        if (alas % 2 == 1) { // Jika alas ganjil
+            cout << "Alas yang dimasukkan adalah bilangan ganjil, luas tidak dapat dihitung." << endl;
+        } else { 
+            float luasSegitiga = luas(alas, tinggi);
+            cout << "Luas Segitiga: " << luasSegitiga << endl;
+        }
+    }
+};
